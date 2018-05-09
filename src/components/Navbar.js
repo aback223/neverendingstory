4121b3e9b6c0e9750f9c49f4a7c53d88bfa77a3f
aback1227 <aback1227@gmail.com>
2018-05-08 21:00:10 -0400

    add dummy links to navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import '../App.css'
 
 const Navbar = () => {
-  return <h1 className="navbar-title">NeverEndingStory</h1>
+  return (
+    <div>
+      <h1 className="navbar-title">NeverEndingStory</h1>
+      <h3><a href="#">Link 1</a></h3>
+      <h3><a href="#">Link 2</a></h3>
+    </div>
+  )
 };
 
 export default Navbar;
