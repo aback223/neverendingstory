cc8d825c98266317175f94b31135c6139f41069b
aback1227 <aback1227@gmail.com>
2018-05-08 20:44:01 -0400

    add dummy text to story input component

---
+++ src/components/StoryInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class StoryInput extends Component {
+  render() {
+    return (
+      <p>this is the story input.</p>
+    )
+  }
+}
+
+export default StoryInput;
