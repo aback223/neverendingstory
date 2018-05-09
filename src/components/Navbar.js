135784eb94e5a60a3c5485d64e1aa9afac9400f8
aback1227 <aback1227@gmail.com>
2018-05-08 21:10:03 -0400

    add navbar classname to outer div in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import '../App.css'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar">
       <h1 className="navbar-title">NeverEndingStory</h1>
       <h3><a href="#">Link 1</a></h3>
       <h3><a href="#">Link 2</a></h3>
