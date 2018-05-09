e783eb37d2bb87599db0a20b9353de9f63f4eda9
aback1227 <aback1227@gmail.com>
2018-05-08 21:33:07 -0400

    add div to separate navbar title and links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,13 @@ import '../App.css'
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1 className="navbar-title">NeverEndingStory</h1>
-      <h3><a href="#">Link 1</a></h3>
-      <h3><a href="#">Link 2</a></h3>
+      <div className="topLeft">
+        <h1 className="navbar-title">NeverEndingStory</h1>
+      </div>
+      <div className="bottom">
+        <h3><a href="#">Link 1</a></h3>
+        <h3><a href="#">Link 2</a></h3>
+      </div>
     </div>
   )
 };
