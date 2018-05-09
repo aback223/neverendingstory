7cc0dd90c25c30287eeaa9b6e5a4cb759b82306a
aback1227 <aback1227@gmail.com>
2018-05-08 23:44:25 -0400

    add switch routes to App Component and move Navbar Component here

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Homepage from './components/Homepage';
+import StoryContainer from './containers/StoryContainer';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Homepage}/>
-        </Switch>
-      </Router>
+      <div className="homepageContainer">
+        <Navbar />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={StoryContainer} />
+          </Switch>
+        </Router>
+      </div>
     );
   }
 }
