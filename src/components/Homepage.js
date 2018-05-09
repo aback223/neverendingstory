d64af394813e5349b1b3d4125cf13c83bb5891ac
aback1227 <aback1227@gmail.com>
2018-05-08 20:28:16 -0400

    remove import of router and switch in Homepage component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
+import StoryContainer from '../containers/StoryContainer'
 
 class Homepage extends Component {
   render() {
