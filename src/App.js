8aedc66fa11739fc72e5d9a425a3cc081873163b
aback1227 <aback1227@gmail.com>
2018-05-08 20:19:43 -0400

    root renders the Homepage component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
 
 class App extends Component {
   render() {
@@ -9,7 +10,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Switch>
-            <Route exact path="/" component={Navbar}/>
+            <Route exact path="/" component={Homepage}/>
           </Switch>
         </div>
       </Router>
