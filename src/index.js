6ce6f49c1028aeaa9e4b130be4ba9590269e3c32
aback1227 <aback1227@gmail.com>
2018-05-09 20:39:27 -0400

    import applyMiddleware from redux in index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 //const rootReducer = combineReducers({usersReducer}); //import and add reducers
 const store = createStore(
+  rootReducer, 
+  applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
