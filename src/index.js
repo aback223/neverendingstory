5b2786516a6c546f97ccced5393b083df87f8cf2
aback1227 <aback1227@gmail.com>
2018-05-09 21:45:21 -0400

    import storyReducer into index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import storyReducer from './reducers/StoryReducer';
 
-const rootReducer = combineReducers({});
 const store = createStore(
-  rootReducer, 
+  storyReducer, 
   applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
