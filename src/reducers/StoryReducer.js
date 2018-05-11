2043fe33d0fb5f047dd4ee1654ca483077f4e1b6
aback1227 <aback1227@gmail.com>
2018-05-10 20:51:08 -0400

    add a case statement for ADD_CONTENT in storyReducer

--- src/reducers/StoryReducer.js
+++ src/reducers/StoryReducer.js
@@ -1,5 +1,7 @@
 export default function storyReducer(state={content:[]}, action) {
   switch(action.type) {
+    case "ADD_CONTENT":
+      return { content: [...state.content, action.content]}
     default: 
       return state
   }
