92d9f354dc70bb026361b15f7f1c9598d7ab4d7c
aback1227 <aback1227@gmail.com>
2018-05-11 00:03:31 -0400

    import connect and define mapStateToProps in Story component

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Story extends Component {
   render() {
     return (
       <div className="bottomContent">
-      Once upon a time...
+        Once upon a time...
       </div>)
   }
 }
 
-export default Story;
+const mapStateToProps = (state) => {
+  return { content: state.content };
+};
+
+export default connect(mapStateToProps)(Story);
