0e7992b411ce60b8464c0bf3696125f905404639
aback1227 <aback1227@gmail.com>
2018-05-10 20:50:29 -0400

    change path of StoryInput import location in StoryContainer

--- src/containers/StoryContainer.js
+++ src/containers/StoryContainer.js
@@ -4,7 +4,6 @@ import StoryInput from './StoryInput';
 
 class StoryContainer extends Component {
   render() {
-    // return <Story contents={this.state.contents} />
     return (
     <div className="storyContainer">
       <div className="topRight"><StoryInput /></div>
