d9822fdd59ec4b88bf7baaeda3d278c191524563
aback1227 <aback1227@gmail.com>
2018-05-10 23:56:11 -0400

    modify return of ADD_CONTENT

--- src/reducers/StoryReducer.js
+++ src/reducers/StoryReducer.js
@@ -1,7 +1,7 @@
 export default function storyReducer(state = { content: [] }, action) {
   switch(action.type) {
     case 'ADD_CONTENT':
-      return state.content.concat(action.content)
+      return { content: state.content.concat(action.content) };
     default: 
       return state;
   }
