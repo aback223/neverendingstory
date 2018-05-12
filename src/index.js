2404679071fb3b1a1d999c64e763866bce35a13c
aback1227 <aback1227@gmail.com>
2018-05-11 22:55:40 -0400

    import compose into index.js for thunk and redux devtools

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import storyReducer from './reducers/StoryReducer';
 
 const store = createStore(
-  storyReducer,
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  storyReducer, 
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), 
+    applyMiddleware(thunk)
+  )
 );
 
 ReactDOM.render(
