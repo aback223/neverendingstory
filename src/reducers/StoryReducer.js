a6cc756adee7a4a735aa33e0f65dc35c45dea539
aback1227 <aback1227@gmail.com>
2018-05-11 23:39:59 -0400

    add a GET_CONTENTS case statement in storyReducer

--- src/reducers/StoryReducer.js
+++ src/reducers/StoryReducer.js
@@ -2,6 +2,8 @@ export default function storyReducer(state = { contents: [] }, action) {
   switch(action.type) {
     case 'ADD_CONTENT':
       return { contents: state.contents.concat(action.content) };
+    case 'GET_CONTENTS':
+      return {...state, contents: action.contents}
     default: 
       return state;
   }
