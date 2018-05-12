e042af105d7065f412058deeef288d94e04db378
aback1227 <aback1227@gmail.com>
2018-05-11 23:31:26 -0400

    modify addContent action to make a post request to rails api

--- src/actions/contents.js
+++ src/actions/contents.js
@@ -1,6 +1,14 @@
 export function addContent(content) {
-  return {
-    type: "ADD_CONTENT",
-    content: content
-  }
+  return (dispatch)  => {
+    return fetch('http://localhost:3000/story_contents', {
+      method: 'POST', 
+      headers: {
+        Accept: 'application/json', 
+        'Content-Type': 'application/json', 
+      }, 
+      body: JSON.stringify(content)
+    })
+    .then(response => response.json())
+    .then(content => dispatch({ type: 'ADD_CONTENT', content: content }));
+  };
 }
