8ba58d0f53e8e6efe31241ee3085a553d1d09b3a
aback1227 <aback1227@gmail.com>
2018-05-12 00:10:48 -0400

    change get_contents to fetch_contents in storyReducer

--- src/reducers/StoryReducer.js
+++ src/reducers/StoryReducer.js
@@ -2,7 +2,7 @@ export default function storyReducer(state = { contents: [] }, action) {
   switch(action.type) {
     case 'ADD_CONTENT':
       return { contents: state.contents.concat(action.content) };
-    case 'GET_CONTENTS':
+    case 'FETCH_CONTENTS':
       return {...state, contents: action.contents}
     default: 
       return state;
