042f0410c4a2acf739c51d5b62bdc001bfc1682c
aback1227 <aback1227@gmail.com>
2018-05-13 12:14:32 -0400

    fix spacing in About component

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const About = ({content}) =>
   <div>
-    
-This is the story that never ends. What will happen next? The fate of this story is in your hands.
+    This is the story that never ends. What will happen next? The fate of this story is in your hands.
   </div>;
 
 export default About;
