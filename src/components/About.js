b48148e2efb4bf91334e98af62b903c963bbf68a
aback1227 <aback1227@gmail.com>
2018-05-13 12:27:26 -0400

    change spacing between sentences in about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const About = ({content}) =>
-  <div>
-    This is the story that never ends. What will happen next? The fate of this story is in your hands.
+  <div className="aboutDiv">
+    This is the story that never ends.<br/><br/> What will happen next? The fate of this story is in your hands.
   </div>;
 
 export default About;
