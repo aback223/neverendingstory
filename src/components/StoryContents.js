050f35a7f1a7847b56ed27ccd96b730eafd29deb
aback1227 <aback1227@gmail.com>
2018-05-17 12:31:27 -0400

    refactor StoryContents component

--- src/components/StoryContents.js
+++ src/components/StoryContents.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Content from './Content';
 
-class StoryContents extends Component {
-  render() {
-    const { contents } = this.props;
-    return (
-      <div className="bottomContent">
-        William Pigeon had always loved industrial Chicago with its cold, confused cliffs. It was a place where he felt puzzled.{"\n\n"}
-        {contents.map(content => <Content content={content} />)}
-      </div>
-    )
-  }
+const StoryContents = ({ contents }) => {
+  return (
+    <div className="bottomContent">
+      William Pigeon had always loved industrial Chicago with its cold, confused cliffs. It was a place where he felt puzzled.{"\n\n"}
+      {contents.map(content => <Content content={content} />)}
+    </div>
+  )
 }
 
 export default StoryContents;
