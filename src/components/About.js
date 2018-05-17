22b50cb678461f1446869122e814826e85178b72
aback1227 <aback1227@gmail.com>
2018-05-17 12:34:34 -0400

    remove the contents as props

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const About = ({content}) =>
+const About = () =>
   <div className="aboutDiv">
     This is the story that never ends.<br/><br/> What will happen next? The fate of this story is in your hands.
   </div>;
--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Feedback = ({content}) =>
+const Feedback = () =>
   <div className="feedbackDiv">
     Github: <a href="https://github.com/aback223/neverendingstory">https://github.com/aback223/neverendingstory</a> <br /><br />
     Feedback, bug reports, and pull requests are welcome on Github at the above link. Thank you!
