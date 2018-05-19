0e1db6e70d5ff26eb069fe84820eae90f7a26822
aback1227 <aback1227@gmail.com>
2018-05-18 22:10:42 -0400

    import connect and bindActionCreators to IndexInput

--- src/containers/IndexInput.js
+++ src/containers/IndexInput.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 class IndexInput extends Component {
   constructor() {
