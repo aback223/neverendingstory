73f70d9e460c01130122057f50e3e201d895043e
aback1227 <aback1227@gmail.com>
2018-05-18 22:09:37 -0400

    add handleChange and handleSubmit methods in Input component

--- src/containers/IndexInput.js
+++ src/containers/IndexInput.js
@@ -7,10 +7,30 @@ class IndexInput extends Component {
       title: ''
     }
   }
+
+  handleChange = event => {
+    this.setState({
+      title: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const title = this.state.title
+    this.props.addStory(title)
+    this.setState({
+      title: ''
+    })
+  }
+
   render() {
     return(
-      <form className="indexInput">
-        <input type="text" placeholder="Title" />
+      <form className="indexInput" onSubmit={this.handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Title"
+          value={this.state.title}
+          onChange={this.handleChange} />
         <input type="submit" value="Create Story" />
       </form>
     )
