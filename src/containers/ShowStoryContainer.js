1a8fea89d2b236fe84466ecd782b683685f56ba7
aback1227 <aback1227@gmail.com>
2018-05-18 23:53:00 -0400

    create a show story container

---
+++ src/containers/ShowStoryContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShowStoryContainer extends Component {
+  render() {
+    return(
+      <div>
+        this is the show story container.
+      </div>
+    )
+  }
+}
+
+export default ShowStoryContainer;
