e844a90f1e58778696a51061a2777bf79d239233
aback1227 <aback1227@gmail.com>
2018-05-18 23:00:37 -0400

    define mapDispatchToProps with fetchStories action

--- src/containers/IndexContainer.js
+++ src/containers/IndexContainer.js
@@ -3,6 +3,7 @@ import IndexInput from './IndexInput';
 import Index from '../components/Index';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { fetchStories } from '../actions/story';
 
 class IndexContainer extends Component {
   render() {
@@ -19,4 +20,10 @@ const mapStateToProps = (state) => {
   return { stories: state.stories}
 }
 
-export default connect(mapStateToProps)(IndexContainer);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    fetchStories: fetchStories
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(IndexContainer);
