e4dcbbef85cb714c88cc33f08d9d5e077880c746
aback1227 <aback1227@gmail.com>
2018-05-19 00:51:16 -0400

    change initial state of story equal to an empty hash

--- src/reducers/StoryReducer.js
+++ src/reducers/StoryReducer.js
@@ -1,7 +1,9 @@
-export default function storyReducer(state = { stories:[], contents: [] }, action) {
+export default function storyReducer(state = { stories:[], story: {}, contents: [] }, action) {
   switch(action.type) {
     case 'FETCH_STORIES':
       return { stories: action.stories }
+    case 'FETCH_STORY':
+      return {...state, story: action.story }
     case 'ADD_CONTENT':
       return { contents: state.contents.concat(action.content) };
     case 'FETCH_CONTENTS':
