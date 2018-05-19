1cf4c209c0cf522c52aae055cc48ad193fc27a5a
aback1227 <aback1227@gmail.com>
2018-05-18 23:03:45 -0400

    define case statement for fetching stories

--- src/reducers/StoryReducer.js
+++ src/reducers/StoryReducer.js
@@ -1,5 +1,7 @@
-export default function storyReducer(state = { contents: [] }, action) {
+export default function storyReducer(state = { stories:[], contents: [] }, action) {
   switch(action.type) {
+    case 'FETCH_STORIES':
+      return { stories: action.stories }
     case 'ADD_CONTENT':
       return { contents: state.contents.concat(action.content) };
     case 'FETCH_CONTENTS':
