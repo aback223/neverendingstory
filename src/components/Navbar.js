4f2fec1ee136f2b33e489e59b443130875df1df0
aback1227 <aback1227@gmail.com>
2018-05-20 16:16:09 -0400

    add a link to stories Index in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ const Navbar = () => {
         <Link to="/" className="navbar-title">Never<br/>Ending<br/>Story</Link> 
       </div>
       <div className="bottomNavbar">
+        <Link to ="/stories">Index</Link>
         <Link to="/about">Intro</Link>
         <Link to="/feedback">Feedback</Link>
       </div>
