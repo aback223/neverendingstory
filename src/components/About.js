f173341bf9bf3f696bc5a94c62f117e555277e59
aback1227 <aback1227@gmail.com>
2018-05-20 18:03:54 -0400

    put a line break after the second sentence in About component

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const About = () =>
   <div className="aboutDiv">
-    This is the story that never ends.<br/><br/> What will happen next? The fate of this story is in your hands.
+    This is the story that never ends.<br/><br/> 
+    What will happen next?<br/><br/> 
+    The fate of this story is in your hands.
   </div>;
 
 export default About;
