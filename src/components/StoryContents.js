e626f67335dde9b27a4a6a055af428f76df89069
aback1227 <aback1227@gmail.com>
2018-05-20 15:54:29 -0400

    render empty string if contents array is empty

--- src/components/StoryContents.js
+++ src/components/StoryContents.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Content from './Content';
 
 const StoryContents = ({ contents }) => {
+
   return (
     <div className="bottomContent">
       William Pigeon had always loved industrial Chicago with its cold, confused cliffs. It was a place where he felt puzzled.{"\n\n"}
-      {contents.map(content => <Content content={content} />)}
+      {contents.length > 0 ? contents.map(content => <Content content={content}/>) : ''}
     </div>
   )
 }
