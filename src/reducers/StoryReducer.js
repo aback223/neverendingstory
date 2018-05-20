c36132ac75b2ea916e256923c45595a68e70d2fa
aback1227 <aback1227@gmail.com>
2018-05-20 17:57:56 -0400

    add a case statement for adding story titles to index

--- src/reducers/StoryReducer.js
+++ src/reducers/StoryReducer.js
@@ -1,9 +1,11 @@
-export default function storyReducer(state = { stories:[], story: {}, contents: [] }, action) {
+export default function storyReducer(state = { stories:[], story: undefined, contents: [] }, action) {
   switch(action.type) {
     case 'FETCH_STORIES':
       return { stories: action.stories }
     case 'FETCH_STORY':
-      return {...state, story: action.story }
+      return { story: action.story}
+    case 'ADD_STORY':
+      return {stories: state.stories.concat(action.title)}
     case 'ADD_CONTENT':
       return { contents: state.contents.concat(action.content) };
     case 'FETCH_CONTENTS':
