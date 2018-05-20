13e28d9ffe3e3a9988757abb07fc97645250e051
aback1227 <aback1227@gmail.com>
2018-05-20 16:25:39 -0400

    link to the show page for a story in the Index component

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Index = ({stories}) => 
   <div className="storyIndex">
-    {stories.map(story => <li>{story.title}</li>)}
+    {stories.map(story => <Link to={"/stories/" + story.id} >{story.title}</Link>)}
   </div>
 
 export default Index;
