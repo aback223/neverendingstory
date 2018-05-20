b0fc3b74040bbe7d376a742d74f1e56ea06710d1
aback1227 <aback1227@gmail.com>
2018-05-20 18:30:26 -0400

    remove import of connect and bindActionCreators in StoryContainer

--- src/containers/StoryContainer.js
+++ src/containers/StoryContainer.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import StoryContents from '../components/StoryContents';
 import StoryInput from './StoryInput';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { fetchContents } from '../actions/contents';
 import PropTypes from 'prop-types';
 
 class StoryContainer extends Component {
-  componentDidMount() {
-    this.props.fetchContents()
-  }
-
   render() {
     return (
     <div className="storyContainer">
-      <div className="topRight"><StoryInput /></div>
+      <div className="topRight">
+        <div className="showStoryTitle">Title: {this.props.title}</div>
+        <StoryInput />
+      </div>
       <StoryContents contents={this.props.contents} />
     </div>
     )
@@ -29,14 +25,4 @@ StoryContainer.propTypes = {
   contents: PropTypes.array
 };
 
-const mapStateToProps = (state) => {
-  return { contents: state.contents };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    fetchContents: fetchContents
-  }, dispatch)
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(StoryContainer);
+export default StoryContainer;
