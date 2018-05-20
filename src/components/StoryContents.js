99ec50a683bfc230da85a0ecfb8e32daaa30f530
aback1227 <aback1227@gmail.com>
2018-05-20 18:15:16 -0400

    remove dummy text from StoryContents

--- src/components/StoryContents.js
+++ src/components/StoryContents.js
@@ -5,7 +5,6 @@ const StoryContents = ({ contents }) => {
 
   return (
     <div className="bottomContent">
-      William Pigeon had always loved industrial Chicago with its cold, confused cliffs. It was a place where he felt puzzled.{"\n\n"}
       {contents.map(content => <Content content={content}/>)}
     </div>
   )
