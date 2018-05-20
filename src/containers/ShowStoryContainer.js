2ecd3ea2f7b0e8f957512a457e7b434da80094b7
aback1227 <aback1227@gmail.com>
2018-05-20 18:44:01 -0400

    remove curly braces from the StoryContainer import in ShowStoryContainer

--- src/containers/ShowStoryContainer.js
+++ src/containers/ShowStoryContainer.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchStoryById } from '../actions/story';
+import StoryContainer from './StoryContainer';
 
 class ShowStoryContainer extends Component {
+  componentDidMount() {
+    this.props.fetchStoryById(this.props.match.params.id)
+  }
+
   render() {
-    return(
-      <div>
-        this is the show story container.
-      </div>
-    )
+    let contents;
+
+    if (this.props.story !== undefined ) {
+      contents = 
+        <div className="showStory">
+          <StoryContainer title={this.props.story.title} contents={this.props.story.contents}/>
+        </div>
+    } else {
+      contents = 
+        <div className="showStory">
+          Nothing to see here. 
+        </div>
+    }
+
+    return contents;
   }
 }
 
-export default ShowStoryContainer;
+const mapStateToProps = (state) => {
+  return { story: state.story }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    fetchStoryById: fetchStoryById
+  }, dispatch)
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowStoryContainer);
