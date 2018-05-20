67db35f052c955aa593f41c6f7862f019b8cee14
aback1227 <aback1227@gmail.com>
2018-05-20 16:27:25 -0400

    line break after every story title in Index

--- src/components/Index.js
+++ src/components/Index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Index = ({stories}) => 
   <div className="storyIndex">
-    {stories.map(story => <Link to={"/stories/" + story.id} >{story.title}</Link>)}
+    {stories.map(story => <Link to={"/stories/" + story.id} >{story.title}<br/></Link>)}
   </div>
 
 export default Index;
