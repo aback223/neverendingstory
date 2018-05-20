42c3fa126f487d761a47a2284eb5ad5519475fa0
aback1227 <aback1227@gmail.com>
2018-05-20 16:05:34 -0400

    remove ternary operator in StoryContents

--- src/components/StoryContents.js
+++ src/components/StoryContents.js
@@ -6,9 +6,10 @@ const StoryContents = ({ contents }) => {
   return (
     <div className="bottomContent">
       William Pigeon had always loved industrial Chicago with its cold, confused cliffs. It was a place where he felt puzzled.{"\n\n"}
-      {contents.length > 0 ? contents.map(content => <Content content={content}/>) : ''}
+      {contents.map(content => <Content content={content}/>)}
     </div>
   )
 }
+
 
 export default StoryContents;
