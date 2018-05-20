e6a4a29cbdf05e819a2e918c864b9272e38c5930
aback1227 <aback1227@gmail.com>
2018-05-20 19:18:29 -0400

    change fetchStory to fetchContents in StoryContainer

--- src/containers/StoryContainer.js
+++ src/containers/StoryContainer.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import StoryContents from '../components/StoryContents';
 import StoryInput from './StoryInput';
 import PropTypes from 'prop-types';
-import { fetchStory } from '../actions/story';
+import { fetchContents } from '../actions/contents';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 class StoryContainer extends Component {
   componentDidMount() {
-    this.props.fetchStory(this.props.storyId)
+    this.props.fetchContents(this.props.storyId)
   }
 
   render() {
@@ -28,7 +28,7 @@ class StoryContainer extends Component {
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
-    fetchStory: fetchStory
+    fetchContents: fetchContents
   }, dispatch)
 }
 
