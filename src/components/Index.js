acfdeafd9c44c04bece6736ad78e75ba68665c2a
aback1227 <aback1227@gmail.com>
2018-05-20 16:10:27 -0400

    remove ternary operator in Index component

--- src/components/Index.js
+++ src/components/Index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Index = ({stories}) => 
   <div className="storyIndex">
-    {stories.length > 0 ? stories.map(story => <li>{story.title}</li>) : ''}
+    {stories.map(story => <li>{story.title}</li>)}
   </div>
 
 export default Index;
