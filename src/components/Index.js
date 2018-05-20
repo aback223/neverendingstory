1881283b834d0b0e4c4d4de0977aec45bdb88900
aback1227 <aback1227@gmail.com>
2018-05-20 15:57:47 -0400

    render an empty string if index doesnt contain stories

--- src/components/Index.js
+++ src/components/Index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Index = ({stories}) => 
   <div className="storyIndex">
-    {stories.map(story => <li>{story.title}</li>)}
+    {stories.length > 0 ? stories.map(story => <li>{story.title}</li>) : ''}
   </div>
 
 export default Index;
