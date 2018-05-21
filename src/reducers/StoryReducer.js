9a70b1f4f0a5ae851f703c8c4d6f90f707cf5747
aback1227 <aback1227@gmail.com>
2018-05-21 01:28:41 -0400

    add case statements for loading_story and fetch_fail

--- src/reducers/StoryReducer.js
+++ src/reducers/StoryReducer.js
@@ -1,15 +1,19 @@
-export default function storyReducer(state = { stories:[], story: undefined, contents: [] }, action) {
+export default function storyReducer(state = { status: "loading", stories:[], story: undefined, contents: [] }, action) {
   switch(action.type) {
     case 'FETCH_STORIES':
       return { stories: action.stories }
+    case 'LOADING_STORY':
+      return { ...state, status: action.status }
     case 'FETCH_STORY':
-      return { story: action.story }
+      return { story: action.story, status: action.status }
+    case 'FETCH_FAIL':
+      return { status: action.status}
     case 'ADD_STORY':
       return { stories: state.stories.concat(action.title) }
     case 'ADD_CONTENT':
-      return {...state, contents: state.contents.concat(action.content) };
+      return { ...state, contents: state.contents.concat(action.content) };
     case 'FETCH_CONTENTS':
-      return {...state, contents: action.contents}
+      return { ...state, contents: action.contents }
     default: 
       return state;
   }
