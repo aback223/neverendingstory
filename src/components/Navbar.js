ef4cbf68db474d7254f26042c193d0f6f48d1f8f
aback1227 <aback1227@gmail.com>
2018-07-31 11:00:50 -0400

    add links for signup and login in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,19 @@ import React from 'react';
 import '../App.css'
 import { Link } from 'react-router-dom';
 
-
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="topLeft">
         <Link to="/" className="navbar-title">Never<br/>Ending<br/>Story</Link> 
       </div>
-      <div className="bottomNavbar">
+      <div className="bottom-navbar">
         <Link to="/">Home</Link>
-        <Link to ="/stories">Stories</Link>
+        <Link to="/stories">Stories</Link>
+      </div>
+      <div class="navbar-login">
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/login">Login</Link>
       </div>
     </div>
   )
