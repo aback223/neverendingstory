95f8e6f7c561d17947158f00157e9fe7ad7e1442
aback1227 <aback1227@gmail.com>
2018-07-31 02:16:16 -0400

    remove feedback component

--- src/components/Feedback.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Feedback = () =>
-  <div className="feedbackDiv">
-    Github: <a href="https://github.com/aback223/neverendingstory">https://github.com/aback223/neverendingstory</a> <br /><br />
-    Feedback, bug reports, and pull requests are welcome on Github at the above link. Thank you!
-  </div>
-
-export default Feedback;
